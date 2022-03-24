96cf4cd63dcf5e82f6de1fa1540e8c0a56a33069
Manuel Herdez <herdez@gmail.com>
2022-03-23 20:38:24 -0600

    base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<div className="bg-teal-700 h-screen">
+			<nav className="bg-black text-white mx-auto py-5 text-3xl text-center shadow-lg">
+				<h1>React whit Tailwindcss</h1>
+			</nav>
+		</div>
+	);
 }
-
-export default App;
